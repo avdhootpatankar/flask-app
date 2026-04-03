e7aa93fb4ee5f4dce5131eede6ec5537649cfd44
avdhootpatankar <patankaravdhoot@gmail.com>
2026-04-03 09:46:32 +0530

    Initial commit

--- users/views.py
+++ users/views.py
@@ -2,6 +2,12 @@ from flask import Blueprint, jsonify
 
 mod = Blueprint('users', __name__, url_prefix='/users')
 
+users = [
+    {"id": 1, "name": "Alice"},
+    {"id": 2, "name": "Bob"},
+    {"id": 3, "name": "Charlie"}
+]
+
 @mod.route('/', methods=['GET'])
 def fetch():
-    return jsonify({"message": "Hello World"})
+    return jsonify(users)
